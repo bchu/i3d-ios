b5663801cf8600599be87f87cd9a68533b8c1969
Brian Chu <bc@brianchu.com>
2014-09-01 21:42:54 -0700

    Ignore pods

--- Pods/Pods-i3d-AFNetworking-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_i3d_AFNetworking : NSObject
-@end
-@implementation PodsDummy_Pods_i3d_AFNetworking
-@end
--- Pods/Pods-i3d-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_i3d : NSObject
-@end
-@implementation PodsDummy_Pods_i3d
-@end
